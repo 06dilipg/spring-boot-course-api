e107a5111348143315ed1e380b9ebd0d5311090a
06dilipg <dilip.g148@gmail.com>
2022-06-06 07:15:05 +0530

    Update App.java

--- src/main/java/io/javabrains/springbootstarter/App.java
+++ src/main/java/io/javabrains/springbootstarter/App.java
@@ -2,10 +2,9 @@ package io.javabrains.springbootstarter;
 
 public class App {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("Hello");
-System.out.println("World");
-	}
+	
+System.out.println("Hello World");
+
+	
 
 }
